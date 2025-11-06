0e92f60360a9894305cc25dae9c3b6490771359b
Ken <bassettkennethj@gmail.com>
2025-11-05 20:45:00 -0500

    The Pickler now includes the millisecond of in the saved object's name so that the same object doesn't overwrite previous versions of itself

--- ezmt/the_pickler.py
+++ ezmt/the_pickler.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import pandas as pd
 
@@ -18,8 +19,9 @@ class ThePickler(json.JSONEncoder):
             return super().default(obj)
         except TypeError:
             # if object is of type dataframe or series, save to a csv
+            file_name = f"{id(obj)}_{int(time.time()*1000)}"
             if isinstance(obj, (pd.DataFrame, pd.Series)):
-                file_name = f"{id(obj)}.csv"
+                file_name += ".csv"
                 path = os.path.join(self.folder, file_name)
                 obj.to_csv(path)
                 return file_name
@@ -28,7 +30,7 @@ class ThePickler(json.JSONEncoder):
 
             try:
                 pickled_data = pickle.dumps(obj)
-                file_name = f"{id(obj)}.pkl"
+                file_name += ".pkl"
                 with open(os.path.join(self.folder, file_name), "wb") as f:
                     f.write(pickled_data)
                 return file_name
